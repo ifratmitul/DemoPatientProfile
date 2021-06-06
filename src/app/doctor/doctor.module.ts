8e333c249ab24d85820721f5d1ed6fa66abc6d13
ifratmitul <ifrat.mitul@hotmail.com>
2021-06-06 17:42:11 +0600

    added footer & copyright

--- src/app/doctor/doctor.module.ts
+++ src/app/doctor/doctor.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DoctorComponent } from './doctor.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 
 @NgModule({
   declarations: [
-    DoctorComponent
+    DoctorComponent,
+    DashboardComponent
   ],
   imports: [
     CommonModule
