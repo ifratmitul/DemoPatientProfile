8f496756fa0542e974b46d9f1522d0613d47cff1
ifratmitul <ifrat.mitul@hotmail.com>
2021-06-06 16:43:58 +0600

    modulize the project & added header

---
+++ src/app/doctor/doctor.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DoctorComponent } from './doctor.component';
+
+
+
+@NgModule({
+  declarations: [
+    DoctorComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DoctorModule { }
---
+++ src/app/prescription/prescription.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PrescriptionComponent } from './prescription.component';
+
+
+
+@NgModule({
+  declarations: [
+    PrescriptionComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PrescriptionModule { }
